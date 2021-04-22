39e120e6623b42e8487f63369f2e6ed2fa5b6aa4
Mark Gray <markgray@marks-air.home>
2021-04-22 21:35:38 +0100

    skills word cloud

--- webapp/main/routes.py
+++ webapp/main/routes.py
@@ -1,6 +1,9 @@
 from flask import current_app, render_template, url_for, redirect, flash, jsonify, request
 from webapp.models import User
 from webapp.main import bp
+from wordcloud import WordCloud
+import base64
+import io
 import config 
 
 @bp.route('/')
@@ -18,7 +21,23 @@ def career():
 
 @bp.route('/skills')
 def skills():
-	return render_template('main/skills.html', title='Skills')
+	soft_skills = 'stakeholder management, presenting, written communication, verbal communication'
+	hard_skills = 'discovery, delivery, story mapping, user testing, AB testing'
+
+	def get_wordcloud(text, colormap):
+		pil_img = WordCloud(height=400, random_state=1, background_color='navy', colormap=colormap).generate(text=text).to_image()
+		img = io.BytesIO()
+		pil_img.save(img, "PNG")
+		img.seek(0)
+		img_b64 = base64.b64encode(img.getvalue()).decode()
+		return img_b64
+
+	
+	
+	soft_skills_cloud = get_wordcloud(soft_skills, 'rainbow')
+	hard_skills_cloud = get_wordcloud(hard_skills, 'magma_r')
+
+	return render_template('main/skills.html', title='Skills', soft_skills_cloud=soft_skills_cloud, hard_skills_cloud=hard_skills_cloud)
 
 @bp.route('/tools')
 def tools():
